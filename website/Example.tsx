37fcb88068b08ebe71cbbabe152694bd3e819f1e
jaywcjlove <398188662@qq.com>
2020-12-27 10:09:51 +0800

    chore: Fix eslint errors.

--- website/Example.tsx
+++ website/Example.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactClock from '../';
 import styles from './exmaple.module.less';
 
-export default () => {
+const Example () => {
   return (
     <div className={styles.clock}>
       <ReactClock />
     </div>
   );
 }
+
+export default Example;
